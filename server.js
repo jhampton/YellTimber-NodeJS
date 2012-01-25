f91bca9ddb55318f3ae05ab6740055848794d8f4
Jeff Hampton <jhampton@Jeffs-MacBook-Air.local>
2012-01-25 02:38:25 -0500

    Initial socket.io test with...fuck...forgot the port.

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (yelltimber-live) is running..');
-}).listen(14006);
+var io = require('socket.io').listen(80);
+
+var chat = io
+  .of('/chat')
+  .on('connection', function (socket) {
+    socket.emit('a message', {
+        that: 'only'
+      , '/chat': 'will get'
+    });
+    chat.emit('a message', {
+        everyone: 'in'
+      , '/chat': 'will get'
+    });
+  });
+
+var news = io
+  .of('/news')
+  .on('connection', function (socket) {
+    socket.emit('item', { news: 'item' });
+  });
