3409b6055d204d95c9137784778251aee41da877
Jeff Hampton <jhampton@Jeffs-MacBook-Air.local>
2012-01-30 03:30:11 -0500

    Seeing if a BIG bunch of code even RUNS

--- server.js
+++ server.js
@@ -17,18 +17,52 @@ var io = require('socket.io').listen(14006);
 /* Good info:
 	Get the client IP form socket.io: var address = socket.handshake.address; or maybe socket.remoteAddress;
 
-
 */
 
 
 // Build functions at the top for the time being.
 // TODO: Refactor functions into a separate libary
-function getSupervisor() {
-	return true;
+
+function getSupervisor(APIKEY) {
+	// Generate a random seed to prepend to the session
+	var seed = Math.floor(Math.random()*10000);
+	var thisMoment = getPreciseTime();
+	
+	seed = seed.toString();
+	thisMoment = thisMoment.toString() + seed;
+		
+	return base_encode(thisMoment);
 }
 
-var crews = [];
-var supervisor = [];
+function getPreciseTime() {
+	return (new Date).getTime();
+}
+
+// Used to generate SHORTCODES from NUMERIC strings ONLY
+function base_encode(num, alphabet) {
+    // http://tylersticka.com/
+    // Based on the Flickr PHP snippet:
+    // http://www.flickr.com/groups/api/discuss/72157616713786392/
+    alphabet = alphabet || '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
+    var base_count = alphabet.length;
+    var encoded = '';
+    while (num >= base_count) {
+        var div = num/base_count;
+        var mod = (num-(base_count*intval(div)));
+        encoded = alphabet.charAt(mod) + encoded;
+        num = intval(div);
+    }
+    if (num) encoded = alphabet.charAt(num) + encoded;
+    return encoded;
+}
+
+// MAIN SOCKET PROCESSING
+
+var foremen = {};
+var apikeys = {'L6GYiEn6NPk3qzvZ': {
+		allowed : true
+	}
+};
 
 var theCompany = io
   .of('/foreman')
@@ -36,18 +70,44 @@ var theCompany = io
 	// A new? crewmember is asking the foreman for a supervisor, so let's generate an ID and get a namespace (supervisor) together
 	// We'll ignore this socket in the future (safe?) and tell the crewmember to report to a supervisor (uri)
 	
-    socket.emit('a message', {
-        that: 'only'
-      , '/chat': 'will get'
-    });
-    theCompany.emit('a message', {
-        everyone: 'in'
-      , '/chat': 'will get'
-    });
+	// Create a new supervisor for this crewmember and attach it to this foreman (APIKEY, TPSID)
+	socket.on('supervisor',function(APIKEY, respondToCrewmember) {
+		console.log('New cremember asking for supervisor for APIKEY: ' + APIKEY);
+		// Make sure the APIKEY is valid, create a new supervisor (session), start listening, and return it.
+		if (apikeys[APIKEY] && apikeys[APIKEY].allowed) {
+			var thisSupervisor = getSupervisor(APIKEY);
+			var supervisorNamespace = APIKEY + '/' + thisSupervisor;
+			// Add a socket.io listener for each crewmember's supervisor
+			// TODO: On Socket.disconnect, remove this item from the foremen object
+			console.log('New cremember reporting to: /' + supervisorNamespace);
+			foremen[APIKEY][thisSupervisor] = io.of('/' + supervisorNamespace)
+			.on('connection',function(socket) {
+				// Tell the client to start sending reports here
+				socket.emit('GetToWork!');
+			})
+			.on('log',function(socket, data) {
+				// Send a message to everyone listening on this namespace EXCEPT the socket
+				socket.broadcast.emit(data);
+				// TODO: Store messages for this Foreman for later analysis
+			});
+			
+			respondToCrewmember(supervisorNamespace);
+		}
+	});
+    // socket.emit('a message', {
+    //         that: 'only'
+    //       , '/foreman': 'You will get a supervisor here.'
+    //     });
+    //     theCompany.emit('a message', {
+    //         everyone: 'in'
+    //       , '/foreman': 'will get'
+    //     });
   });
 
-var news = io
-  .of('/news')
-  .on('connection', function (socket) {
-    socket.emit('item', { news: 'item' });
-  });
+console.log('The Company is listening.');
+console.log(apikeys);
+// var news = io
+//   .of('/news')
+//   .on('connection', function (socket) {
+//     socket.emit('item', { news: 'item' });
+//   });
